5a15f2b5b31863cdfca1d319b07d0a27d1e658c9
TooSmoove <keithadrainsellers@gmail.com>
2024-07-28 14:54:47 -0400

    Add files via upload

---
+++ ClarityEmailAPI/Controllers/ClarityEmailController.cs
@@ -0,0 +1,75 @@
+﻿using ClarityEmailAPI.Email;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Nodes;
+
+namespace ClarityEmailAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ClarityEmailController : ControllerBase
+    {
+        private readonly ILogger<ClarityEmailController> _logger;
+        private IConfiguration _configuration;
+        public ClarityEmailController(ILogger<ClarityEmailController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [EnableCors]
+        [AllowAnonymous]
+        [HttpPost("ClarityEmailSend")]
+        public  async Task<string> SendClarityEmail(JsonObject requestOptions)
+        {
+            JsonObject RequestOptions = requestOptions;
+            string result = "";
+            string path = _configuration.GetSection("LocalSettings").GetSection("EmailLogFile").Value;
+
+            string From = RequestOptions["From"].ToString();
+            string To = RequestOptions["To"].ToString();
+            string Message = RequestOptions["Message"].ToString();
+            string Subject = RequestOptions["Subject"].ToString();
+
+            using (StreamWriter writer = new FileInfo(path).AppendText())
+            {
+                writer.WriteLine("Date: " + DateTime.Now);
+                writer.WriteLine("From: " + From);
+                writer.WriteLine("To: " + To);
+                writer.WriteLine("Subject: " + Subject);
+                writer.WriteLine("Message: " + Message);
+                writer.WriteLine("\n");
+                writer.WriteLine("/*********************************");
+            }
+
+
+            ClarityEmail email = new ClarityEmail(_configuration);
+
+            for (int tries = 0; tries < 4; tries++)
+            {
+                try
+                {
+                    await email.SendEmailAsync(From, Message, To, Subject);
+                    result = "Email sent!";
+                    break;
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e.Message);
+
+                    if (tries == 3)
+                    {
+                        result = "Email failed 3 times; check your addess and try again";
+                        break;
+                    }
+
+                    result = "Email Error";
+                }
+            }
+            
+
+            return result;
+        }
+    }
+}
---
+++ ClarityEmailAPI/Email/ClarityEmail.cs
@@ -0,0 +1,80 @@
+﻿using Amazon.SimpleEmail;
+using Amazon.SimpleEmail.Model;
+
+namespace ClarityEmailAPI.Email
+{
+    public class ClarityEmail
+    {
+        private static IConfiguration config;
+
+        // Replace recipient@example.com with a "To" address. If your account
+        // is still in the sandbox, this address must be verified.
+        static string receiverAddress;
+
+        private readonly ILogger<ClarityEmail> _logger;
+
+        public ClarityEmail(IConfiguration configuration)
+        {
+            config = configuration;
+            receiverAddress = config.GetSection("LocalSettings").GetSection("ccAddress").Value;
+        }
+
+        public ClarityEmail(ILogger<ClarityEmail> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<string> SendEmailAsync(string emailFrom, string emailBody, string emailTo, string emailSubject)
+        {
+            var _amazonSimpleEmailService = new AmazonSimpleEmailServiceClient();
+            var messageId = "";
+            List<string> bccAddresses = new List<string>() { receiverAddress };
+            List<string> ccAddresses = new List<string>();
+            List<string> toAddresses = new List<string>() { emailTo };
+
+            try
+            {
+                var response = await _amazonSimpleEmailService.SendEmailAsync(
+                    new SendEmailRequest
+                    {
+                        Destination = new Destination
+                        {
+                            BccAddresses = bccAddresses,
+                            CcAddresses = ccAddresses,
+                            ToAddresses = toAddresses
+                        },
+                        Message = new Message
+                        {
+                            Body = new Body
+                            {
+                                Html = new Content
+                                {
+                                    Charset = "UTF-8",
+                                    Data = emailBody
+                                },
+                                Text = new Content
+                                {
+                                    Charset = "UTF-8",
+                                    Data = emailBody
+                                }
+                            },
+                            Subject = new Content
+                            {
+                                Charset = "UTF-8",
+                                Data = emailSubject
+                            }
+                        },
+                        Source = emailFrom
+                    });
+                messageId = response.MessageId;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("SendEmailAsync failed with exception: " + ex.Message);
+            }
+
+            return messageId;
+            
+        }
+    }
+}
